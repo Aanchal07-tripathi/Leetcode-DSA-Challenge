c0e7b987f9f64ce08e7089bff8994de8d1d9552d
Aanchal Tripathi <tripathiaanchal68@gmail.com>
2026-08-07 23:01:38 +0530

    Time: 2 ms (98.98%), Space: 46.7 MB (15.6%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
@@ -29,3 +29,30 @@ class Solution {
         return suffix;
     }
 }
+//Optimized
+// class Solution {
+//     public int[] leftRigthDifference(int[] nums) {
+
+//         int n = nums.length;
+//         int[] answer = new int[n];
+
+//         int total = 0;
+
+//         for (int num : nums) {
+//             total += num;
+//         }
+
+//         int leftSum = 0;
+
+//         for (int i = 0; i < n; i++) {
+
+//             int rightSum = total - leftSum - nums[i];
+
+//             answer[i] = Math.abs(leftSum - rightSum);
+
+//             leftSum += nums[i];
+//         }
+
+//         return answer;
+//     }
+// }
